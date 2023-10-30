3120fc91d44ba60401982d6f5df92116c27b6992
Axel Glöckner <axel.glockner00@gmail.com>
2023-10-30 23:18:33 +0100

    textwatcher

--- Laboration2/app/src/main/java/com/example/laboration2/MainActivity.java
+++ Laboration2/app/src/main/java/com/example/laboration2/MainActivity.java
@@ -1,114 +1,154 @@
-package com.example.laboration2;
+    package com.example.laboration2;
 
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
-import android.widget.Toast;
+    import android.graphics.Color;
+    import android.os.Bundle;
+    import android.text.Editable;
+    import android.text.TextWatcher;
+    import android.util.Log;
+    import android.view.View;
+    import android.widget.EditText;
+    import android.widget.ExpandableListAdapter;
+    import android.widget.ExpandableListView;
+    import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+    import java.util.ArrayList;
+    import java.util.HashMap;
+    import java.util.List;
+    import java.util.Map;
+    import java.util.StringTokenizer;
 
-public class MainActivity extends AppCompatActivity {
-    String groupAdress ="";
-    String childAdress = "";
-    EditText input;
-    List<String> groupList;
-    List<String> childList;
-    Map<String, List<String>> collection;
-    ExpandableListView expandableListView;
-    ExpandableListAdapter expandableListAdapter;
+    public class MainActivity extends AppCompatActivity {
+        String groupAdress ="";
+        String childAdress = "";
+        EditText input;
+        List<String> groupList;
+        List<String> childList;
+        Map<String, List<String>> collection;
+        ExpandableListView expandableListView;
+        ExpandableListAdapter expandableListAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        createGroupList();
-        createCollection();
-        input = findViewById(R.id.input);
-        expandableListView = findViewById(R.id.labb2);
-        expandableListAdapter = new MyExpandableListAdapter(this, groupList, collection);
-        expandableListView.setAdapter(expandableListAdapter);
-        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
-            @Override
-            public void onGroupExpand(int i) {
-                String selected = expandableListAdapter.getGroup(i).toString();
-                groupAdress = selected;
-                input.setText("/" + groupAdress + "/");
-                Toast.makeText(getApplicationContext(),
-                        expandableListAdapter.getGroup(i) + " List Expanded.", Toast.LENGTH_SHORT).show();
-            }
-        });
-        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
-            @Override
-            public void onGroupCollapse(int i) {
-                String selected = expandableListAdapter.getGroup(i).toString();
-                if(selected == groupAdress){
-                    input.setText("");
-                    return;
-                }else{
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            createGroupList();
+            createCollection();
+            input = findViewById(R.id.input);
+            expandableListView = findViewById(R.id.labb2);
+            expandableListAdapter = new MyExpandableListAdapter(this, groupList, collection);
+            expandableListView.setAdapter(expandableListAdapter);
+            expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+                @Override
+                public void onGroupExpand(int i) {
+                    String selected = expandableListAdapter.getGroup(i).toString();
                     groupAdress = selected;
-                            input.setText("/" + groupAdress + "/");
+                    input.setText("/" + groupAdress + "/");
+                    Toast.makeText(getApplicationContext(),
+                            expandableListAdapter.getGroup(i) + " List Expanded.", Toast.LENGTH_SHORT).show();
                 }
-                Toast.makeText(getApplicationContext(),
-                        expandableListAdapter.getGroup(i) + " List Collapsed.", Toast.LENGTH_SHORT).show();
-            }
-        });
+            });
+            expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+                @Override
+                public void onGroupCollapse(int i) {
+                    String selected = expandableListAdapter.getGroup(i).toString();
+                    if(selected == groupAdress){
+                        input.setText("");
+                        return;
+                    }else{
+                        groupAdress = selected;
+                                input.setText("/" + groupAdress + "/");
+                    }
+                    Toast.makeText(getApplicationContext(),
+                            expandableListAdapter.getGroup(i) + " List Collapsed.", Toast.LENGTH_SHORT).show();
+                }
+            });
 
-        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-            @Override
-            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
-                String selected = expandableListAdapter.getChild(i, i1).toString(); // grouppos, childpos.
-                //Toast.makeText(getApplicationContext(), "Selected" + " " + selected, Toast.LENGTH_SHORT).show();
-                childAdress = selected;
-                groupAdress = expandableListAdapter.getGroup(i).toString();
-                String tmp = "/" + groupAdress + "/" + childAdress + "/";
-                input.setText(tmp);
+            expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+                @Override
+                public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+                    String selected = expandableListAdapter.getChild(i, i1).toString(); // grouppos, childpos.
+                    //Toast.makeText(getApplicationContext(), "Selected" + " " + selected, Toast.LENGTH_SHORT).show();
+                    childAdress = selected;
+                    groupAdress = expandableListAdapter.getGroup(i).toString();
+                    String tmp = "/" + groupAdress + "/" + childAdress + "/";
+                    input.setText(tmp);
+                    return true;
+                }
+            });
+            input.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+                @Override
+                public void afterTextChanged(Editable editable) {
+                    String searchText = editable.toString();
+                    if (isSearchTextValid(searchText)) {
+                        input.setBackgroundColor(Color.GREEN);
+                    } else {
+                        input.setBackgroundColor(Color.RED);
+                    }
+                }
+            });
+        }
+        private void createCollection() {
+            String[] data = {"green", "yellow", "red", "blue"};
+            collection = new HashMap<String, List<String>>();
+            for(String group: groupList){
+                loadChild(data);
+                collection.put(group, childList);
+            }
+        }
+        private void loadChild(String[] data) {
+            childList = new ArrayList<>();
+            for(String color : data){
+                childList.add(color);
+            }
+        }
+        private void createGroupList() {
+            groupList = new ArrayList<>();
+            groupList.add("light");
+            groupList.add("medium");
+            groupList.add("dark");
+        }
+
+        public static List<String> splitter(String input) {
+            List<String> resultList = new ArrayList<>();
+
+            // Split the input string by "/"
+            String[] parts = input.split("/");
+
+            // Add the elements to the list, including the trailing "/"
+            for (String part : parts) {
+                if (!part.isEmpty()) {
+                    resultList.add("/");
+                    resultList.add(part);
+                }
+            }
+
+            // Add the trailing "/" if it exists in the original input
+            if (input.endsWith("/")) {
+                resultList.add("/");
+            }
+
+            return resultList;
+        }
+        private boolean isSearchTextValid(String searchText) {
+            List<String> string = splitter(searchText.trim());
+            if (string.size() == 3 && "/"==(string.get(0)) && groupList.contains(string.get(1)) && "/"==(string.get(2))) {
+                return true;
+            } if (string.size() == 5 && "/" == string.get(0) && groupList.contains(string.get(1)) && "/" == string.get(2)
+                  && childList.contains(string.get(3)) && string.get(4) == "/") {
                 return true;
             }
-        });
-
-        input.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-    }
-    private void createCollection() {
-        String[] data = {"green", "yellow", "red", "blue"};
-        collection = new HashMap<String, List<String>>();
-        for(String group: groupList){
-            loadChild(data);
-            collection.put(group, childList);
+            return false;
         }
+
     }
-    private void loadChild(String[] data) {
-        childList = new ArrayList<>();
-        for(String color : data){
-            childList.add(color);
-        }
-    }
-    private void createGroupList() {
-        groupList = new ArrayList<>();
-        groupList.add("light");
-        groupList.add("medium");
-        groupList.add("dark");
-    }
-}
--- Laboration2/app/src/main/java/com/example/laboration2/MyExpandableListAdapter.java
+++ Laboration2/app/src/main/java/com/example/laboration2/MyExpandableListAdapter.java
@@ -6,41 +6,48 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class MyExpandableListAdapter extends BaseExpandableListAdapter {
+public class MyExpandableListAdapter extends BaseExpandableListAdapter implements Filterable {
     private Context context;
     private Map<String, List<String>> collection;
     private List<String> groupList;
+    private List<String> filteredGroupList;
+    private List<String> filteredChildList;
 
     public MyExpandableListAdapter(Context context, List<String> groupList,
-                                   Map<String, List<String>> collection){
+                                   Map<String, List<String>> collection) {
         this.context = context;
         this.groupList = groupList;
         this.collection = collection;
-
+        this.filteredGroupList = new ArrayList<>(groupList);
+        this.filteredChildList = new ArrayList<>();
     }
+
     @Override
     public int getGroupCount() {
-        return collection.size();
+        return filteredGroupList.size();
     }
+
     @Override
     public int getChildrenCount(int i) {
-        return collection.get(groupList.get(i)).size();
+        return collection.get(filteredGroupList.get(i)).size();
     }
+
     @Override
     public Object getGroup(int i) {
-        return groupList.get(i);
+        return filteredGroupList.get(i);
     }
 
     @Override
     public Object getChild(int i, int i1) {
-        return collection.get(groupList.get(i)).get(i1);
+        return collection.get(filteredGroupList.get(i)).get(i1);
     }
 
     @Override
@@ -61,7 +68,7 @@ public class MyExpandableListAdapter extends BaseExpandableListAdapter {
     @Override
     public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
         String name = getGroup(i).toString();
-        if(view == null){
+        if (view == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.group_item, null);
         }
@@ -74,7 +81,7 @@ public class MyExpandableListAdapter extends BaseExpandableListAdapter {
     @Override
     public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
         String model = getChild(i, i1).toString();
-        if(view == null){
+        if (view == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.child_item, null);
         }
@@ -82,8 +89,42 @@ public class MyExpandableListAdapter extends BaseExpandableListAdapter {
         item.setText(model);
         return view;
     }
+
     @Override
     public boolean isChildSelectable(int i, int i1) {
         return true;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                FilterResults filterResults = new FilterResults();
+                List<String> filteredGroups = new ArrayList<>();
+                List<String> filteredChildren = new ArrayList<>();
+
+                for (String group : groupList) {
+                    for (String child : collection.get(group)) {
+                        String combined = "/" + group + "/" + child + "/";
+                        if (combined.contains(charSequence)) {
+                            filteredGroups.add(group);
+                            filteredChildren.add(child);
+                        }
+                    }
+                }
+
+                filterResults.values = filteredGroups;
+                filterResults.count = filteredGroups.size();
+                filteredChildList = filteredChildren;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredGroupList = (List<String>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
